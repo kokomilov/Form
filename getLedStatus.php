c855ce8c4f5555d9c0e15229593b63ba1b037c04
kokomilov <163173540+kokomilov@users.noreply.github.com>
2024-12-02 18:07:32 -0600

    Add files via upload

---
+++ getLedStatus.php
@@ -0,0 +1,33 @@
+<?php
+header("ngrok-skip-browser-warning:0");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "esp32_control";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//retrieve LED status
+$sql = "SELECT * FROM led_status WHERE id = 1";
+$result = $conn->query($sql);
+
+if ($result) {
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo json_encode($row);
+    } else {
+        echo json_encode(["error" => "No data found"]);
+    }
+} else {
+    echo json_encode(["error" => "Query failed"]);
+}
+
+// Close the connection
+$conn->close();
+?>
